83a552979258fc52672a358f6940bfeadfd3d37a
Meet Chauhan <108236696+ChauhanMeet7@users.noreply.github.com>
2025-03-08 20:19:08 +0530

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+//using WebApplication1.netCore.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
+
